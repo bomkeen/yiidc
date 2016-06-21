3807909f49fe5c3554b4d48dc16e3ba377e66572
tehnn <tehnnn@gmail.com>
2016-06-21 20:42:50 +0700

    yiidc

--- frontend/views/site/about.php
+++ frontend/views/site/about.php
@@ -1,5 +1,4 @@
 <?php
-
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
@@ -8,9 +7,11 @@ $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
+    <?php
+    if (!\Yii::$app->user->isGuest) {
+        echo \Yii::$app->user->identity->role;
+    }  else {
+        echo "ไม่ได้ล้อกอิน";
+    }
+    ?>
 </div>
