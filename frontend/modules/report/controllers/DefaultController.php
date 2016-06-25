983fedd25e45852fe76d7ceb461106b782e8269a
tehnn <tehnnn@gmail.com>
2016-06-25 19:25:48 +0700

    utehn

--- frontend/modules/report/controllers/DefaultController.php
+++ frontend/modules/report/controllers/DefaultController.php
@@ -9,53 +9,53 @@ use dosamigos\arrayquery\ArrayQuery;
 /**
  * Default controller for the `report` module
  */
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * Renders the index view for the module
      * @return string
      */
     public $enableCsrfValidation = false;
-    
-    public function actionIndex($find=NULL)
-    {
+
+    public function actionIndex($find = NULL) {
         $sql = "  SELECT p.CID,p.`NAME`,p.LNAME
 ,GROUP_CONCAT(c.CHRONIC) as CHRONIC
 FROM  chronic c
 INNER JOIN person p 
 ON p.HOSPCODE = c.HOSPCODE AND p.PID = c.PID
-GROUP BY c.HOSPCODE,c.PID   " ;
-                
+GROUP BY c.HOSPCODE,c.PID   ";
+
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
         /* เพิ่มเข้ามาจากวันแรก */
         $query = new ArrayQuery($raw);
         $model = $query->find();
-        if(!empty($find)){
-        $model = $query
-                ->addCondition("CID", "like $find", "or")
-                ->addCondition("NAME", "like $find","or" )
-                ->addCondition("LNAME","like $find")
-                ->find();        }   
-            
-       /* เพิ่มเข้ามาจากวันแรก */
-        
+        if (!empty($find)) {
+            $model = $query
+                    ->addCondition("CID", "like $find", "or")
+                    ->addCondition("NAME", "like $find", "or")
+                    ->addCondition("LNAME", "like $find")
+                    ->find();
+        }
+
+        /* เพิ่มเข้ามาจากวันแรก */
+
         if (!empty($raw[0])) {
             $cols = array_keys($raw[0]);
         }
-        
-        $dataProvider =new ArrayDataProvider([
-            'allModels'=>$model, // เปลี่ยนเป็น model
+
+        $dataProvider = new ArrayDataProvider([
+            'allModels' => $model, // เปลี่ยนเป็น model
             'sort' => !empty($cols) ? [ 'attributes' => $cols] : FALSE,
-            'pagination'=>[
-                'pageSize'=>15
+            'pagination' => [
+                'pageSize' => 15
             ]
-         
         ]);
-        
-        
-        return $this->render('index',[
-            'dataProvider'=>$dataProvider,
-            'find'=>$find
+
+
+        return $this->render('index', [
+                    'dataProvider' => $dataProvider,
+                    'find' => $find
         ]);
     }
+
 }
