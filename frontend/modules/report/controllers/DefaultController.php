dc552bbcfc2e628670655cb65ae55bf82b9ebe26
tehnn <tehnnn@gmail.com>
2016-06-25 19:24:38 +0700

    utehn

--- frontend/modules/report/controllers/DefaultController.php
+++ frontend/modules/report/controllers/DefaultController.php
@@ -17,7 +17,7 @@ class DefaultController extends Controller
      */
     public $enableCsrfValidation = false;
     
-    public function actionIndex()
+    public function actionIndex($find=NULL)
     {
         $sql = "  SELECT p.CID,p.`NAME`,p.LNAME
 ,GROUP_CONCAT(c.CHRONIC) as CHRONIC
@@ -27,27 +27,35 @@ ON p.HOSPCODE = c.HOSPCODE AND p.PID = c.PID
 GROUP BY c.HOSPCODE,c.PID   " ;
                 
         $raw = \Yii::$app->db->createCommand($sql)->queryAll();
-        
-       
-        
-       
+        /* เพิ่มเข้ามาจากวันแรก */
+        $query = new ArrayQuery($raw);
+        $model = $query->find();
+        if(!empty($find)){
+        $model = $query
+                ->addCondition("CID", "like $find", "or")
+                ->addCondition("NAME", "like $find","or" )
+                ->addCondition("LNAME","like $find")
+                ->find();        }   
+            
+       /* เพิ่มเข้ามาจากวันแรก */
         
         if (!empty($raw[0])) {
             $cols = array_keys($raw[0]);
         }
         
         $dataProvider =new ArrayDataProvider([
-            'allModels'=>$raw,
+            'allModels'=>$model, // เปลี่ยนเป็น model
             'sort' => !empty($cols) ? [ 'attributes' => $cols] : FALSE,
             'pagination'=>[
-                'pageSize'=>100
+                'pageSize'=>15
             ]
          
         ]);
         
         
         return $this->render('index',[
-            'dataProvider'=>$dataProvider
+            'dataProvider'=>$dataProvider,
+            'find'=>$find
         ]);
     }
 }
--- frontend/modules/report/views/default/index.php
+++ frontend/modules/report/views/default/index.php
@@ -1,13 +1,34 @@
 <?php
 use kartik\grid\GridView;
 use yii\widgets\ActiveForm;
+use yii\widgets\Pjax;
+use yii\helpers\Html;
+
+$this->params['breadcrumbs'][] = ['label' => 'หน้าหลัก', 'url' => ['/site/index']];
+$this->params['breadcrumbs'][] = 'ผลงานคัดกรองรายหน่วยบริการ';
 ?>
 
-<?php
+<?php Pjax::begin(['id' => 'my-pjax', 'timeout' => 10000]) ?>
 
+<div class="_search" style="margin-bottom: 5px">
 
+    <?php $form = ActiveForm::begin([
+        'action' => ['/report/default/index'],
+        'method' => 'get',
+        'options' => ['data-pjax' => true ]
+    ]); ?>
+    <div class="input-group">
+      <?= Html::textInput('find',$find,['placeholder'=>"ค้นหา..","class"=>"form-control"]) ?>
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="submit">
+            <i class="glyphicon glyphicon-search"></i> ค้นหา
+        </button>
+       
+      </span>
+    </div>
+    <?php ActiveForm::end(); ?>
 
-?>
+</div>
 
 
 <?php
