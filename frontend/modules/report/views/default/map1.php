fbe810073882e347619ca6a7fdbecea648b0673f
tehnn <tehnnn@gmail.com>
2016-06-26 13:46:56 +0700

    utehn

--- frontend/modules/report/views/default/map1.php
+++ frontend/modules/report/views/default/map1.php
@@ -8,11 +8,30 @@ $this->registerJsFile("https://api.mapbox.com/mapbox.js/v2.4.0/mapbox.js", ['pos
 </div>
 <?php
 $js=<<<JS
-   
-
+ 
 L.mapbox.accessToken = 'pk.eyJ1IjoidGVobm5uIiwiYSI6ImNpZzF4bHV4NDE0dTZ1M200YWxweHR0ZzcifQ.lpRRelYpT0ucv1NN08KUWQ';
 
-L.mapbox.map('map', 'mapbox.streets');
+var map = L.mapbox.map('map', 'mapbox.streets');
+
+map.setView([16,100],6);
+var point_layer;
+        
+$.getJSON('./gis/point.json',function(data){
+    
+       point_layer = L.geoJson(data,{
+           onEachFeature:function(feature,layer){
+           
+            layer.setIcon(L.mapbox.marker.icon({'marker-color': '#ffe101'})); 
+            
+            layer.bindPopup(feature.properties.TAM_NAMT);
+           }
+       
+       }).addTo(map);
+       map.fitBounds(point_layer.getBounds());
+});
+
+       
+        
         
 JS;
 
